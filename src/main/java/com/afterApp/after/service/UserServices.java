70f68d52078cd0e328eb10488326ab366701f9d0
Ethan Lorente <ethanlorente@MBP-de-Ethan.home>
2026-07-08 18:56:53 +0200

    Refactor: add UserLoader

--- src/main/java/com/afterApp/after/service/UserServices.java
+++ src/main/java/com/afterApp/after/service/UserServices.java
@@ -9,6 +9,7 @@ import com.afterApp.after.exceptions.AlreadyExistsException;
 import com.afterApp.after.exceptions.BadRequestException;
 import com.afterApp.after.exceptions.FormatRequestException;
 import com.afterApp.after.exceptions.NotFoundException;
+import com.afterApp.after.loader.UserLoader;
 import com.afterApp.after.repositories.UserAccessRepository;
 import com.afterApp.after.repositories.UserRepository;
 import com.afterApp.after.utils.TokenUtil;
@@ -21,20 +22,19 @@ import java.util.List;
 public class UserServices {
     @Autowired
     UserRepository userRepository;
-
     @Autowired
     private TokenUtil tokenUtil;
-
     @Autowired
     private UserAccessRepository userAccessRepository;
+    @Autowired
+    UserLoader userLoader;
 
 
     private Users extractUser(String authorization){
         String jwt = authorization.replace("Bearer ", "");
         String username = tokenUtil.extractUsername(jwt);
 
-        UserAccess userAccess = userAccessRepository.findByUsername(username)
-                .orElseThrow(() -> new NotFoundException("User not Found"));
+        UserAccess userAccess = userLoader.findByUsername(username);
 
         return userAccess.getUser();
     }
@@ -59,7 +59,7 @@ public class UserServices {
     }
 
     public Users getUserEntityById(Long id) throws RuntimeException{
-        return userRepository.findById(id).orElseThrow(() -> new NotFoundException("User not found"));
+        return userLoader.findById(id);
     }
 
     public UserResponseDTO updateUser(Long id, UpdateUserDTO uDtoDetails, String authorization){
@@ -76,7 +76,7 @@ public class UserServices {
         if(uDtoDetails.getPhoneNumber() != null) {u.setPhoneNumber(uDtoDetails.getPhoneNumber());}
         if(uDtoDetails.getEmail() != null) {u.setEmail(uDtoDetails.getEmail());}
 
-        return toDto(userRepository.save(u)); // mapper + unitTest
+        return toDto(userLoader.saveUser(u)); // mapper + unitTest
     }
 
     public UserResponseDTO updateDisplayName(Long id, String authorization, UpdateDisplayNameDTO uDetails){
@@ -90,13 +90,11 @@ public class UserServices {
 
         String newDisplayName = uDetails.getDisplayName();
 
-        if(userRepository.existsByDisplayName(uDetails.getDisplayName())){
-            throw new AlreadyExistsException("Already Existing username");
-        }
+        userLoader.existByDisplayName(uDetails);
 
         u.setDisplayName(newDisplayName);
 
-        return toDto(userRepository.save(u));
+        return toDto(userLoader.saveUser(u));
     }
 
 
