aae6239e811fd786053e9819073e3a7840550506
Ethan Lorente <ethanlorente@MacBook-Pro-de-Ethan.local>
2026-08-10 14:44:12 +0200

    Error response DTO & GlobalEx. Refactor

--- src/main/java/com/afterApp/after/controller/GlobalExceptionHandler.java
+++ src/main/java/com/afterApp/after/controller/GlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.afterApp.after.controller;
 
+import com.afterApp.after.dto.ErrorResponseDTO;
+import com.afterApp.after.exceptions.NotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,20 +27,42 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<Map<String, String>> handleInvalidJson(
-            HttpMessageNotReadableException exception) {
+    public ResponseEntity<ErrorResponseDTO> handleInvalidJson(
+            HttpMessageNotReadableException exception, HttpServletRequest request) {
+        ErrorResponseDTO errorResponse = ErrorResponseDTO.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .message("Invalid request body")
+                .path(request.getRequestURI())
+                .build();
 
-        return ResponseEntity
-                .badRequest()
-                .body(Map.of("error", "Invalid request body"));
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<Map<String, String>> handleInternalServerError(RuntimeException exception){
-        return ResponseEntity
-                .internalServerError()
-                .body(Map.of("error", "Unexpected server error"));
+    public ResponseEntity<ErrorResponseDTO> handleInternalServerError(RuntimeException exception, HttpServletRequest request){
+       ErrorResponseDTO errorResponse = ErrorResponseDTO.builder()
+               .timestamp(LocalDateTime.now())
+               .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+               .error(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+               .message("Unexpected Server Error")
+               .path(request.getRequestURI())
+               .build();
+
+       return ResponseEntity.internalServerError().body(errorResponse);
     }
-    //token catch
-    //not found
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> handleNotFound(NotFoundException exception, HttpServletRequest request){
+        ErrorResponseDTO errorResponse = ErrorResponseDTO.builder()
+                .status(HttpStatus.NOT_FOUND.value())
+                .error(HttpStatus.NOT_FOUND.getReasonPhrase())
+                .message("Content not Found")
+                .path(request.getRequestURI())
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
 }
